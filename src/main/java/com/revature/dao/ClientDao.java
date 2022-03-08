edd0d98cd620a03c755acea72997c4cccf9dfe33
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-03-08 14:57:10 -0500

    clientDao

---
+++ src/main/java/com/revature/dao/ClientDao.java
@@ -0,0 +1,29 @@
+    package com.revature.dao;
+
+    import com.revature.model.Client;
+
+    import java.util.List;
+
+    //TODO 6: Create a Dao (data access object) for a particular 'entity'
+    public class ClientDao {
+
+    //TODO 8 : Create the method for the CRUD operations
+    //return type of Client because we want to actually return back to whoever is trying to add th client record the record
+    // that they add themselves
+    //C
+    public Client addClients(Client client) {
+        return null;
+    }
+
+    //R
+    public Client getClientsById(Client client) {
+        return null;
+    }
+
+    public List<Client> getAllClients() {
+        return null;
+    }
+    //U
+        public Client updateClient(Client clients)
+
+}
--- src/main/java/com/revature/dao/StudentDao.java
+++
@@ -1,7 +0,0 @@
-package com.revature.dao;
-
-//TODO 6: Create a Dao (data access object) for a particular 'entity'
-public class StudentDao {
-
-
-}
