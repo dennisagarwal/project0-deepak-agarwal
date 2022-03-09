5ae1d64ba260fc962a7f50af5f7f6609c0efe394
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-03-09 15:47:53 -0500

    adding javalin implementation and controller

--- src/main/java/com/revature/main/Driver.java
+++ src/main/java/com/revature/main/Driver.java
@@ -1,8 +1,10 @@
 package com.revature.main;
 
+import com.revature.controller.Controller;
 import com.revature.dao.ClientDao;
 import com.revature.model.Client;
 import com.revature.utility.ConnectionUtility;
+import io.javalin.Javalin;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,11 +12,26 @@ import java.sql.SQLException;
 public class Driver {
 
     public static void main(String[] args) {
-        ClientDao clientDao = new ClientDao();
+        //        ClientDao clientDao = new ClientDao();
+        //this willl give us the Javalin object
+        //the javalin object is the server
+        Javalin app = Javalin.create();
 
-        try{
-            Client c = new Client(5, "Rad", "Doe", 31);
-            System.out.println(clientDao.updateClient(c));
+        mapControllers(app);
+
+        app.start(); //port 8080 by default
+    }
+//we create controller interface
+
+    public static void mapControllers(Javalin app, Controller... controllers ) {
+        //loop through each of the controller and called map endpoint on them
+       for(Controller c : controllers){
+           c.mapEndpoints(app);
+    }
+
+//        try{
+            //            Client c = new Client(5, "Rad", "Doe", 31);
+//            System.out.println(clientDao.updateClient(c));
 //            System.out.println(clientDao.addClient(c));
 //
 //            System.out.println(clientDao.getClientById(1));
@@ -23,9 +40,9 @@ public class Driver {
 //            System.out.println(clientDao.deleteClientById(3));
 //            System.out.println(clientDao.getAllClients());
 //            System.out.println(clientDao.getAllClients());
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+//        } catch (SQLException e){
+//            e.printStackTrace();
+//        }
 
 }
 }
